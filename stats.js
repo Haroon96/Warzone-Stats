beecd69591f9e6003d365c63a2ac21cf912efc90
Muhammad Haroon <m_haroon96@hotmail.com>
2020-05-18 02:31:12 +0500

    added initialvalue to reduce + removed username from matches list

--- stats.js
+++ stats.js
@@ -1,8 +1,8 @@
 module.exports = {
-    getDailyStats: getDailyStats
+    getStats: getStats
 };
 
-const codApi = require('./cod-api');
+const getRecentMatches = require('./cod-api').getRecentMatches;
 const moment = require('moment');
 
 function calculateStats(matches) {
@@ -10,27 +10,24 @@ function calculateStats(matches) {
     let sum = (a, b) => a + b;
     let statValues = {
         'Matches': stats.length,
-        'Kills': stats.map(x => x.kills.value).reduce(sum),
-        'Deaths': stats.map(x => x.deaths.value).reduce(sum),
-        'Score': stats.map(x => x.score.value).reduce(sum),
-        'Time Played': moment.utc(stats.map(x => x.timePlayed.value).reduce(sum) * 1000).format('HH:mm:ss'),
-        'Headshots': stats.map(x => x.headshots.value).reduce(sum),
-        'Assists': stats.map(x => x.assists.value).reduce(sum),
-        'Executions': stats.map(x => x.executions.value).reduce(sum),
-        'Vehicles Destroyed': stats.map(x => x.objectiveDestroyedVehicleMedium ? x.objectiveDestroyedVehicleMedium.value : 0).reduce(sum),
-        'Team Wipes': stats.map(x => x.objectiveTeamWiped ? x.objectiveTeamWiped.value : 0).reduce(sum),
-        'Total XP': stats.map(x => x.totalXp.value).reduce(sum)
+        'Kills': stats.map(x => x.kills.value).reduce(sum, 0),
+        'Deaths': stats.map(x => x.deaths.value).reduce(sum, 0),
+        'Score': stats.map(x => x.score.value).reduce(sum, 0),
+        'Time Played': moment.utc(stats.map(x => x.timePlayed.value).reduce(sum, 0) * 1000).format('HH:mm:ss'),
+        'Headshots': stats.map(x => x.headshots.value).reduce(sum, 0),
+        'Assists': stats.map(x => x.assists.value).reduce(sum, 0),
+        'Executions': stats.map(x => x.executions.value).reduce(sum, 0),
+        'Vehicles Destroyed': stats.map(x => x.objectiveDestroyedVehicleMedium ? x.objectiveDestroyedVehicleMedium.value : 0).reduce(sum, 0),
+        'Team Wipes': stats.map(x => x.objectiveTeamWiped ? x.objectiveTeamWiped.value : 0).reduce(sum, 0),
+        'Total XP': stats.map(x => x.totalXp.value).reduce(sum, 0)
     }
     statValues['K/D'] = statValues.Kills / statValues.Deaths;
     statValues['K/D'] = statValues['K/D'].toFixed(2);
 
-    return {
-        username: matches[0].segments[0].metadata.platformUserHandle,
-        stats: statValues,
-    }
+    return statValues;
 }
 
-async function getDailyStats(platform, username) {
-    let matches = await codApi.getTodaysMatches(platform, username);
+async function getStats(platform, username, duration) {
+    let matches = await getRecentMatches(platform, username, duration);
     return calculateStats(matches);
 }
