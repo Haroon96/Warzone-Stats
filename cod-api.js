0a59b8fdba7a3290d78b1243feb010f7a1ed0cb1
Muhammad Haroon <m_haroon96@hotmail.com>
2020-06-23 15:40:55 +0500

    removed early return from api

--- cod-api.js
+++ cod-api.js
@@ -44,13 +44,6 @@ async function getRecentMatches(platform, username, duration) {
         let res = await request(url);
 
         if (res.errors) {
-            // return recentMatches if previous calls were successful
-            // an error probably indicates end of matches for this user
-            if (recentMatches.length > 0) {
-                return recentMatches;
-            }
-            // if there are no matches saved from previous calls,
-            // throw the error
             throw {msg: res.errors[0].message, code: res.errors[0].code};
         }
 
