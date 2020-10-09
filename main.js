2695d6c987eb94afa46910a389b6d5de5c09f2db
Muhammad Haroon <m_haroon96@hotmail.com>
2020-10-09 20:30:15 +0500

    added main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+const discord = require('discord.js');
+const bot = new discord.Client();
+
+const db = require('./db');
+const controller = require('./controller');
+const scheduler = require('./scheduler');
+
+init();
+
+// run all setup tasks and then start discord bot
+async function init() {
+    await db.init();
+    await scheduler.init(bot);
+    await initBot();    
+}
+
+async function initBot() {
+    // login to bot
+    bot.login(process.env.TOKEN);
+
+    // run when ready
+    bot.on('ready', () => {
+        bot.user.setActivity({name: "for '!wz' commands", type: "WATCHING"});
+        console.info(`Logged in as ${bot.user.tag}`);
+    });
+    
+    // run when message received
+    bot.on('message', async(msg) => {
+        // only respond to messages starting with !wz
+        if (!msg.content.startsWith('!wz')) {
+            return;
+        }
+
+        console.log(`[${new Date().toISOString()}] [${msg.author.username}] ${msg.content}`);
+        
+        // forward to controller
+        controller(msg);
+    });    
+}
+
