7cb7ec2c69ca96283ac8aa7dde4d6fcd63a6754f
Muhammad Haroon <m_haroon96@hotmail.com>
2021-02-13 02:14:18 +0500

    added dal

---
+++ src/dal/mongo-dal.ts
@@ -0,0 +1,34 @@
+import { Db, MongoClient } from "mongodb";
+import { Player, Guild, GameMode } from "../common/types";
+
+
+class MongoDAL {
+    
+    async getGuildPlayers(guildId: string): Promise<Array<Player>> {
+        let guild: Guild = await this.db.collection('guilds').findOne({ guildId });
+
+        // if guild not found in db, create it
+        if (guild == null) {
+            guild = { guildId, players: [] };
+            await this.db.collection('guilds').insertOne(guild);
+        }
+        return guild.players;
+    }
+
+    async getModeIds(mode: GameMode): Promise<Array<string>> {
+        return (await this.db.collection('modes').findOne({ mode })).modeIds;
+    }
+
+    constructor() {
+        let self = this;
+        MongoClient
+        .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(client => {
+            self.db = client.db(process.env.MONGO_DBNAME);
+        });
+    }
+
+    db: Db = null;
+}
+
+export const DAL = new MongoDAL();
