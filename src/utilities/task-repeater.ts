12ac81e532eadff7eb3bb76fd0af1233100a51a4
Muhammad Haroon <m_haroon96@hotmail.com>
2021-06-02 14:03:32 -0700

    implemented retry notifications

--- src/utilities/task-repeater.ts
+++ src/utilities/task-repeater.ts
@@ -1,33 +1,44 @@
 export default class TaskRepeater {
 
-    constructor(taskFn, args, delay, retries) {
+    constructor(taskFn: Function, args: Array<any>, delay: number, retries: number, onError: Function = null) {
         this.taskFn = taskFn;
         this.args = args;
         this.delay = delay;
         this.retries = retries;
+        this.onError = onError;
     }
 
     async run() {
-        for (let i = 0; i < this.retries; ++i) {
+        let error = null;
+        for (let i = 1; i <= this.retries; ++i) {
             try {
+                // call repeater function
                 return await this.taskFn(...this.args);
             } catch (e) {
-                this.error = e;
+                // onError callback
+                if (this.onError) {
+                    this.onError(e, i, this.retries);
+                }
+                // set error
+                error = e;
+                // wait for timeout
                 await this.timeout();
             }
         }
-        throw this.error;
+        throw error;
     }
 
     timeout() {
         return new Promise(resolve => {
+            // increase delay time for each error
+            this.delay *= 2;
             setTimeout(resolve, this.delay);
         });
     }
 
     taskFn: Function;
-    args: Array<string>;
+    args: Array<any>;
     delay: number;
     retries: number;
-    error: any;
+    onError: Function;
 }
