f099fa183de50b3684ca45247d74760d969ea1c3
Osama Rana <ohrules@gmail.com>
2022-03-20 21:47:44 +0500

    EOL message

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -4,6 +4,31 @@ import commandMap from "./mapping.js";
 import { updateAnalyticsSheet } from "../utilities/analytics.js"
 
 export async function controller(message: Message) {
+    const eolMsg = `Since May 2020, this bot has helped Warzone players keep track of their stats. What started as a collaboration between 3 friends turned into an active side project as more and more players organically found our bot.
+
+**However, we're sad to announce that the bot is shutting down. The bot will cease operations on 1st of April, 2022.**
+
+This is partly due to the increasing complexity of using the Tracker API, and partly because of lack of time. 
+
+We'd like to extend our thanks to all the people who added the bots to their discord servers, people who opened issues & PRs on the Github repo, and even forked the repo to translate it to other languages. It was truly a pleasure to see our open source project grow.
+
+As a parting gift, here are some stats about the bot:
+\`\`\`
+Peak concurrent discord servers: 246
+Total discord servers: ~350
+Commands processed by bot: 21904 since May 2021
+Most commands by a single discord server: 4702
+Registered players: 687
+Game modes tracked: 48
+Schedules set: 26
+\`\`\`
+If you'd like to take a look at the source code, and maybe even setup your own bot using the COD API, check out https://github.com/Haroon96/warzone-stats.
+
+Bye :)
+`;
+
+    await message.reply(eolMsg);
+    return;
 
     // sanitize the message body
     message.content = trimWhitespace(message.content).toLowerCase();
@@ -23,9 +48,6 @@ export async function controller(message: Message) {
     if (process.env.ENABLE_ANALYTICS) {
         await updateAnalyticsSheet(commandName, message.guild.id);    
     }
-
-    await message.reply("The bot is under maintenance, sorry for the inconvenience! You can get in touch with us on github: https://github.com/Haroon96/warzone-stats");
-    return;
     
     // check if command regex matches
     for (const regex of command.regex) {
